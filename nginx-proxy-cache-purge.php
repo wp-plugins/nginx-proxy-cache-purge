f1f6d66b69ab3aebf6259d69b76e7a6ce4a4e6a5
jlevandowski <jlevandowski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-23 03:44:56 +0000

    tagging version 0.9.3
    git-svn-id: https://plugins.svn.wordpress.org/nginx-proxy-cache-purge/tags/0.9.3@279505 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nginx-proxy-cache-purge.php
+++ nginx-proxy-cache-purge.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: Nginx Proxy Cache Purge
 Plugin URI: http://wpselect.com/
-Description: This plugin will purge the nginx proxy cache when you publish or update a post or page.
-Version: 0.9.1
+Description: Purges the nginx proxy cache when you publish or update a post or page.
+Version: 0.9.3
 Author: John Levandowski
 Author URI: http://johnlevandowski.com/
 */
 
-function wpselect_cache() {
+function wpselect_cache($post_id) {
 #post/page purge url
 $link = get_permalink($post_id);
 $parse = parse_url($link);
@@ -23,10 +23,12 @@ if ($parse_home[path] != '') {
 } else $home_page_url = $home_page_url.'/';
 
 #posts page purge url
+if ( get_option('show_on_front') == 'page' ) {
 $posts_page = get_option('page_for_posts');
 $posts_page_link = get_permalink($posts_page);
 $parse_posts = parse_url($posts_page_link);
 $posts_url = $parse_posts[scheme].'://'.$parse_posts[host].'/purge'.$parse_posts[path];
+} ;
 
 #feed purge url
 $feed_url = $home_page_url.'feed/';
@@ -51,4 +53,17 @@ foreach ($urls_unique as $uri) {
 }
 
 add_action('edit_post', 'wpselect_cache');
+
+function wpselect_footer() {
+	$content = '<!-- Page created in ';
+	$content .= timer_stop($display = 0, $precision = 2);
+	$content .= ' seconds from ';
+	$content .= get_num_queries();
+	$content .= ' queries on ';
+	$content .= date('m.d.y \@ H:i:s T');
+	$content .= ' -->';
+	echo $content;
+}
+
+add_action('wp_footer', 'wpselect_footer');
 ?>
